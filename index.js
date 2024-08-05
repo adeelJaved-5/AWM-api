04cdcca81f39d88df7502f19360cc8a22475f98b
Adeel Javed <adeel.javed@vividly.it>
2024-08-05 22:16:31 +0500

    first commit

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ mongoose.set("strictQuery", false);
 mongoose.connect(MONGO_URL);
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: false }));
 app.use(logger("dev"));
 app.use(
   express.urlencoded({
