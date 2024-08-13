397703531790e9b75d3dcaab4dfbd392fcc2d79b
Adeel Javed <adeel.javed@vividly.it>
2024-08-13 11:53:25 +0500

    Update

--- controllers/paint/paint.validator.js
+++ controllers/paint/paint.validator.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 const paintUpdate = {
   body: Joi.object({
-    hitlineClasses: Joi.array().items(Joi.string()).required(),
+    hitlineClasses: Joi.string().required(),
   }),
 };
 
