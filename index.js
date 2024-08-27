d8e17dd4e52af1d784c987882b4231bbbd23c623
Adeel Javed <adeel.javed@vividly.it>
2024-08-27 14:36:24 +0500

    update

--- index.js
+++ index.js
@@ -16,13 +16,7 @@ mongoose.connect(MONGO_URL);
 
 app.use(cors());
 app.use(express.json({ limit: "200mb" }));
-app.use(express.urlencoded({ limit: "200mb", extended: false }));
-app.use(logger("dev"));
-app.use(
-  express.urlencoded({
-    extended: false,
-  })
-);
+app.use(express.urlencoded({ limit: "200mb", extended: true, parameterLimit: 1000000 }));
 
 app.use("/", require("./routes"));
 
