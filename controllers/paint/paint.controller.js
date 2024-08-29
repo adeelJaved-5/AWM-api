2fe6ac28a328332ea3881115dd03a8b342f8a66e
Adeel Javed <adeel.javed@vividly.it>
2024-08-29 15:28:59 +0500

    update

--- controllers/paint/paint.controller.js
+++ controllers/paint/paint.controller.js
@@ -1,24 +1,34 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 
 const paintDataPath = path.join(__dirname, 'paintData.json');
 
-const readPaintData = () => {
-  if (!fs.existsSync(paintDataPath)) {
-    return null;
+const readPaintData = async () => {
+  try {
+    await fs.access(paintDataPath); // Check if the file exists
+    const data = await fs.readFileSync(paintDataPath, 'utf-8');
+    return JSON.parse(data);
+  } catch (error) {
+    // If the error is due to the file not existing, return null; otherwise, throw the error
+    if (error.code === 'ENOENT') {
+      return null;
+    } else {
+      throw error;
+    }
   }
-
-  const data = fs.readFileSync(paintDataPath, 'utf-8');
-  return JSON.parse(data);
 };
 
-const writePaintData = (data) => {
-  fs.writeFileSync(paintDataPath, JSON.stringify(data, null, 2), 'utf-8');
+const writePaintData = async (data) => {
+  try {
+    await fs.writeFileSync(paintDataPath, JSON.stringify(data, null, 2), 'utf-8');
+  } catch (error) {
+    throw error;
+  }
 };
 
 exports.storePaint = async ({ hitlineClasses }) => {
   try {
-    let paint = readPaintData();
+    let paint = await readPaintData();
 
     if (paint) {
       const mergedHitlineClasses = [...new Set([...paint.hitlineClasses, ...hitlineClasses])];
@@ -27,20 +37,20 @@ exports.storePaint = async ({ hitlineClasses }) => {
       paint = { hitlineClasses };
     }
 
-    writePaintData(paint);
+    await writePaintData(paint);
     return { status: "success", message: "Paint updated successfully" };
   } catch (error) {
     console.log('An error occurred while updating paint:', error);
-    throw new Error(error);
+    throw new Error('An error occurred while updating paint');
   }
 };
 
 exports.getPaint = async () => {
   try {
-    const paint = readPaintData();
+    const paint = await readPaintData();
     return paint;
   } catch (error) {
     console.log('An error occurred while retrieving paint:', error);
-    throw new Error(error);
+    throw new Error('An error occurred while retrieving paint');
   }
 };
