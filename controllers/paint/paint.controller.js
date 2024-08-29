23b0522fb617a3a7129802215e0c99d5e0587dae
Adeel Javed <adeel.javed@vividly.it>
2024-08-29 16:28:37 +0500

    update

--- controllers/paint/paint.controller.js
+++ controllers/paint/paint.controller.js
@@ -1,34 +1,24 @@
-const fs = require('fs').promises;
+const fs = require('fs');
 const path = require('path');
 
 const paintDataPath = path.join(__dirname, 'paintData.json');
 
-const readPaintData = async () => {
-  try {
-    await fs.access(paintDataPath); // Check if the file exists
-    const data = await fs.readFile(paintDataPath, 'utf-8');
-    return JSON.parse(data);
-  } catch (error) {
-    // If the error is due to the file not existing, return null; otherwise, throw the error
-    if (error.code === 'ENOENT') {
-      return null;
-    } else {
-      throw error;
-    }
+const readPaintData = async() => {
+  if (!fs.existsSync(paintDataPath)) {
+    return null;
   }
+
+  const data = await fs.readFileSync(paintDataPath, 'utf-8');
+  return JSON.parse(data);
 };
 
-const writePaintData = async (data) => {
-  try {
-    await fs.writeFile(paintDataPath, JSON.stringify(data, null, 2), 'utf-8');
-  } catch (error) {
-    throw error;
-  }
+const writePaintData = async(data) => {
+  await fs.writeFileSync(paintDataPath, JSON.stringify(data, null, 2), 'utf-8');
 };
 
 exports.storePaint = async ({ hitlineClasses }) => {
   try {
-    let paint = await readPaintData();
+    let paint = readPaintData();
 
     if (paint) {
       const mergedHitlineClasses = [...new Set([...paint.hitlineClasses, ...hitlineClasses])];
@@ -41,7 +31,7 @@ exports.storePaint = async ({ hitlineClasses }) => {
     return { status: "success", message: "Paint updated successfully" };
   } catch (error) {
     console.log('An error occurred while updating paint:', error);
-    throw new Error('An error occurred while updating paint');
+    throw new Error(error);
   }
 };
 
@@ -51,6 +41,6 @@ exports.getPaint = async () => {
     return paint;
   } catch (error) {
     console.log('An error occurred while retrieving paint:', error);
-    throw new Error('An error occurred while retrieving paint');
+    throw new Error(error);
   }
 };
