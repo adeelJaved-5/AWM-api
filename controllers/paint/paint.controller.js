dd28bad66125a7bcb1e1138d7b71033395c7bc42
Adeel Javed <adeel.javed@vividly.it>
2024-10-25 16:49:08 +0500

    Update

--- controllers/paint/paint.controller.js
+++ controllers/paint/paint.controller.js
@@ -5,13 +5,18 @@ const paintDataPath = path.join(__dirname, 'paintData.json');
 
 const readPaintData = async () => {
   try {
-    if (!await fs.promises.access(paintDataPath, fs.constants.F_OK).then(() => true).catch(() => false)) {
+    const exists = await fs.promises.access(paintDataPath, fs.constants.F_OK)
+      .then(() => true)
+      .catch(() => false);
+      
+    if (!exists) {
       return {};
     }
+    
     const data = await fs.promises.readFile(paintDataPath, 'utf-8');
     return data ? JSON.parse(data) : {};
   } catch (error) {
-    console.log('Error parsing JSON data:', error);
+    console.error('Error parsing JSON data:', error);
     return {};
   }
 };
@@ -21,38 +26,33 @@ const writePaintData = async (data) => {
     const jsonData = JSON.stringify(data, null, 2);
     await fs.promises.writeFile(paintDataPath, jsonData, 'utf-8');
   } catch (error) {
-    console.log('Error writing paint data:', error);
-    throw new Error(error);
+    console.error('Error writing paint data:', error);
+    throw new Error('Failed to write data');
   }
 };
 
 exports.storePaint = async ({ hitlineClasses }) => {
+  if (!Array.isArray(hitlineClasses)) {
+    throw new Error("Invalid data format: 'hitlineClasses' should be an array.");
+  }
+
   try {
     let paintData = await readPaintData();
-
-    if (paintData) {
-      const mergedHitlineClasses = [...new Set([...paintData.hitlineClasses, ...hitlineClasses])];
-      paintData.hitlineClasses = mergedHitlineClasses;
-    } else {
-      paintData = { hitlineClasses };
-    }
-
+    paintData.hitlineClasses = [...new Set([...(paintData.hitlineClasses || []), ...hitlineClasses])];
     await writePaintData(paintData);
-
+    
     return { status: "success", message: "Paint updated successfully" };
   } catch (error) {
-    console.log('An error occurred while updating paint:', error);
-    throw new Error(error);
+    console.error('An error occurred while updating paint:', error);
+    throw new Error('Update failed');
   }
 };
 
-exports.getPaint = async (id) => {
+exports.getPaint = async () => {
   try {
-    const paintData = await readPaintData();
-    return paintData;
+    return await readPaintData();
   } catch (error) {
-    console.log('An error occurred while retrieving paint:', error);
-    throw new Error(error);
+    console.error('An error occurred while retrieving paint:', error);
+    throw new Error('Failed to retrieve paint data');
   }
 };
-
